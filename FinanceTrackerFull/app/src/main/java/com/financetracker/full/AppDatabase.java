73807b0ec051fe646a6ac3f2008029300b072c3e
skytekd <xaran00@gmail.com>
2025-11-25 22:27:46 +0100

    Delete FinanceTrackerFull directory

--- FinanceTrackerFull/app/src/main/java/com/financetracker/full/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.financetracker.full;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Transaction.class}, version = 1)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract TransactionDao transactionDao();
-}
